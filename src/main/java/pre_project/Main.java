b450312df7a1ff4674284fc2d1f8df6b2f9b7e5a
Igor <47169805+IgorDevR@users.noreply.github.com>
2022-09-07 18:29:25 +0300

    +

--- src/main/java/pre_project/Main.java
+++ src/main/java/pre_project/Main.java
@@ -1,5 +1,7 @@
-package pre_project;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+package pre_project;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
 }
